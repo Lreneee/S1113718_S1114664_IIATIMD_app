d02f431be44d7a3a2a5b78fd60134df3621016f4
Lrene <irenedekoning1@gmail.com>
2020-07-07 10:58:32 +0200

    ballbala

--- app/src/main/java/com/example/iiatimdapp/MainActivity.java
+++ app/src/main/java/com/example/iiatimdapp/MainActivity.java
@@ -10,17 +10,10 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
-
-import com.android.volley.toolbox.StringRequest;
-
 import com.example.iiatimdapp.Room.Moestuin;
 import com.example.iiatimdapp.Room.HandleTokenTask;
 
-import com.example.iiatimdapp.Room.MoestuinMaten;
 import com.example.iiatimdapp.Room.Zaadjes;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -29,17 +22,13 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.viewpager.widget.ViewPager;
-
 import com.example.iiatimdapp.Room.Token;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "APIcall";
@@ -63,75 +52,75 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
 
         zaadjes = VolleySingleton.getInstance(this).getZaadjes();
-
-        setContentView(R.layout.activity_login);
-
-        final Response.Listener<String> loginResponse = new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    JSONObject json = new JSONObject(response);
-
-                    String accessToken = json.getString("access_token");
-                    String refreshToken = json.getString("refresh_token");
-                    Token token = new Token(accessToken, refreshToken);
-                    AppDatabase db = AppDatabase.getInstance(getApplicationContext());
-                    new Thread(new HandleTokenTask(db, token)).start();
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                Intent myIntent = new Intent(MainActivity.this, HomeActivity.class);
-                MainActivity.this.startActivity(myIntent);
-            }
-        };
-
-        Button btnLogin = (Button) findViewById(R.id.btnLogin);
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                final EditText usernameField = (EditText) findViewById(R.id.userName);
-                final String username = usernameField.getText().toString();
-                final EditText passwordField = (EditText) findViewById(R.id.password);
-                final String password = passwordField.getText().toString();
-
-                if (username.length() > 0 && password.length() > 0) {
-                    APIManager.getInstance(getApplicationContext()).loginRequest(username, password,loginResponse);
-                }
-
-            }
-        });
-
-        Button btnRegister = (Button) findViewById(R.id.btnRegister);
-
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                Log.d("MainActivity", "Button clicked");
-                final EditText usernameField = (EditText) findViewById(R.id.userName);
-                final String username = usernameField.getText().toString();
-                final EditText passwordField = (EditText) findViewById(R.id.password);
-                final String password = passwordField.getText().toString();
-
-                if (username.length() > 0 && password.length() > 0) {
-
-                    APIManager.getInstance(getApplicationContext()).registerRequest(username, password,
-                            new Response.Listener<String>() {
-                                @Override
-                                public void onResponse(String response) {
-                                    Log.d("MainActivity", "response");
-                                    APIManager.getInstance(getApplicationContext()).loginRequest(username, password, loginResponse);
-                                }
-                            });
-                }
-            }
-        });
+//
+//        setContentView(R.layout.activity_login);
+//
+//        final Response.Listener<String> loginResponse = new Response.Listener<String>() {
+//            @Override
+//            public void onResponse(String response) {
+//                try {
+//                    JSONObject json = new JSONObject(response);
+//
+//                    String accessToken = json.getString("access_token");
+//                    String refreshToken = json.getString("refresh_token");
+//                    Token token = new Token(accessToken, refreshToken);
+//                    AppDatabase db = AppDatabase.getInstance(getApplicationContext());
+//                    new Thread(new HandleTokenTask(db, token)).start();
+//
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                Intent myIntent = new Intent(MainActivity.this, HomeActivity.class);
+//                MainActivity.this.startActivity(myIntent);
+//            }
+//        };
+//
+//        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+//
+//        btnLogin.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//
+//                final EditText usernameField = (EditText) findViewById(R.id.userName);
+//                final String username = usernameField.getText().toString();
+//                final EditText passwordField = (EditText) findViewById(R.id.password);
+//                final String password = passwordField.getText().toString();
+//
+//                if (username.length() > 0 && password.length() > 0) {
+//                    APIManager.getInstance(getApplicationContext()).loginRequest(username, password,loginResponse);
+//                }
+//
+//            }
+//        });
+//
+//        Button btnRegister = (Button) findViewById(R.id.btnRegister);
+//
+//        btnRegister.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//
+//                Log.d("MainActivity", "Button clicked");
+//                final EditText usernameField = (EditText) findViewById(R.id.userName);
+//                final String username = usernameField.getText().toString();
+//                final EditText passwordField = (EditText) findViewById(R.id.password);
+//                final String password = passwordField.getText().toString();
+//
+//                if (username.length() > 0 && password.length() > 0) {
+//
+//                    APIManager.getInstance(getApplicationContext()).registerRequest(username, password,
+//                            new Response.Listener<String>() {
+//                                @Override
+//                                public void onResponse(String response) {
+//                                    Log.d("MainActivity", "response");
+//                                    APIManager.getInstance(getApplicationContext()).loginRequest(username, password, loginResponse);
+//                                }
+//                            });
+//                }
+//            }
+//        });
 
 
     }
